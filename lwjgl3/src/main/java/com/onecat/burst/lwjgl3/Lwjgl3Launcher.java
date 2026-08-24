2ffb275512f8df00add8c7f4774a41b9b5b5d576
damnedpie <sb26@ya.ru>
2026-08-25 06:23:00 +1000

    Burst liftoff

---
+++ lwjgl3/src/main/java/com/onecat/burst/lwjgl3/Lwjgl3Launcher.java
@@ -0,0 +1,29 @@
+package com.onecat.burst.lwjgl3;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.onecat.burst.Main;
+
+/** Launches the desktop (LWJGL3) application. */
+public class Lwjgl3Launcher {
+
+	public static void main(String[] args) {
+		if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
+		createApplication();
+	}
+
+	private static Lwjgl3Application createApplication() {
+		return new Lwjgl3Application(new Main(), getDefaultConfiguration());
+	}
+
+	private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+		Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+		configuration.setTitle("Burst Editor");
+		configuration.useVsync(true);
+		configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
+		configuration.setWindowedMode(640, 480);
+		configuration.setWindowIcon("logo512.png", "logo256.png", "logo128.png", "logo64.png", "logo32.png", "logo16.png");
+		return configuration;
+	}
+
+}
